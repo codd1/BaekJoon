d6f4cf72c983e1bc3ae5b8c5725986473137c326
CYHONG_PC <mgs08041@naver.com>
2024-03-13 01:04:42 +0900

    자료구조 - 1253번 ('좋은 수' 구하기) - 해결

--- DataStructure/1253.cpp
+++ DataStructure/1253.cpp
@@ -4,28 +4,23 @@
 using namespace std;
 
 int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int N;		// 수의 갯수
-	int input[2000] = { 0, };	// 1 <= A <= 2000
+	int A[2000] = { 0, };	// 1 <= A <= 2000
 	int count = 0;
 
 	cin >> N;
 
 	for (int i = 0; i < N; i++) {
-		cin >> input[i];
+		cin >> A[i];
 	}
 
 	// 정렬
-	sort(input, input + N);
-
-	//int A[2000] = { input[0] };
-
-	//// 중복 숫자 제거
-	//for (int inputIndex = 0, AIndex = 0; inputIndex < N; inputIndex++) {
-	//	if (input[inputIndex] != A[AIndex]) {
-	//		AIndex++;
-	//		A[AIndex] = input[inputIndex];
-	//	}
-	//}
+	sort(A, A + N);
 
 	if (N < 3) {
 		cout << count << endl;
@@ -33,42 +28,50 @@ int main() {
 		return 0;
 	}
 
-	int first = 0, second = 1;
-	int index = N - 1;
-	int sum = input[first] + input[second];
+	int first = 1, second = 2;
+	int index = 0;
+	int sum = A[first] + A[second];
 
-	// 음수의 경우 고려필요, first++ 없음
-	// 5
-	// -1 -1 0 1 1
-	while (index >= 0) {
-		if (sum < input[index] && second <= index + 1) {
-			second++;
-			sum = input[first] + input[second];
-		}
-		else {
-			if (sum == input[index]) {
-				if (second == index) {
-					second++;
-					sum = input[first] + input[second];
-					continue;
-				}
-				else if (first == index) {
-					first = 1;
-					second = 2;
-					sum = input[first] + input[second];
-					continue;
-				}
-				else {
-					count++;
-					printf("%d : first = %d, second = %d, sum = %d\n", index, first, second, sum);
-				}
+	// 투포인터
+	while (index < N) {
+		if (sum > A[index]) {
+			if (first == N - 2) {	// 다 비교해도 '좋은 수'가 되지 않는 경우
+				index++;
+				first = 0;
+				second = 1;
 			}
-
-			first = 0;
-			second = 1;
-			index--;
-			sum = input[first] + input[second];
+			else {
+				first++;
+				second = first + 1;
+			}
 		}
+		else if (sum < A[index]) {
+			if (second == N - 1) {	// 마지막 수까지 비교한 경우 first++
+				first++;
+				second = first + 1;
+			}
+			else {
+				second++;
+			}
+		}
+		else {		// sum == A[index]
+			if (first == index) {			// 자기 자신이 더해진 경우
+				first++;
+				second = first + 1;
+			}
+			else if (second == index) {		// 자기 자신이 더해진 경우
+				second++;
+			}
+			else {		// '좋은 수'인 경우
+				count++;
+				//printf("===%d : first = %d, second = %d, sum = %d\n", index, first, second, sum);
+				index++;
+				first = 0;
+				second = 1;
+			}
+		}
+		sum = A[first] + A[second];
+		//printf("%d : first = %d, second = %d, sum = %d\n", index, first, second, sum);
 	}
 
 	cout << count << endl;
