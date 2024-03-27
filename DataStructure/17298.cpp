3fb7dcbc19dcda5a661b290d7ff28716fb72d6cf
CYHONG_PC <mgs08041@naver.com>
2024-03-28 04:34:10 +0900

    자료구조 - 17298번 (오큰수) - 해결

--- DataStructure/17298.cpp
+++ DataStructure/17298.cpp
@@ -12,60 +12,33 @@ int main() {
 	int N;		// 수열의 크기 N
 	stack<int> stack;
 
-	vector<int> NGE(1000001);
-	vector<int> A(1000001);
+	vector<int> NGE(1000001, -1);	// 기본적으로 -1로 초기화
+	vector<int> A(1000001, 0);
 
 	cin >> N;
 
-	int max = 0;
-	for (int i = 1; i <= N; i++) {
+	for (int i = 0; i < N; i++) {
 		cin >> A[i];
-		stack.push(A[i]);
-
-		if (A[i] > max) {
-			max = A[i];
-		}
 	}
 
-	NGE[N] = -1;
+	stack.push(0);
+	
+	int stack_size = 0;
 
-	// A = [ 3, 5, 2, 7 ]
-	// B = [ 4 3 2 1 2 3 4 ]
-
-	for (int i = N - 1; i > 0; i--) {
-		if (A[i] < stack.top()) {		// A) 3 < 5
-			NGE[i] = stack.top();
-		}
-		else {
-			if (A[i] < NGE[i + 1]) {	// A) 5 < 2(NGE(3)=7)
-				NGE[i] = NGE[i + 1];
-			}
-			else {
-				if (A[i] == max) {
-					NGE[i] = -1;
-				}
-				else {		// B) 2 = 1(NGE(4)=2)
-					for (int j = i + 2; j <= N; j++) {
-						if (A[i] < A[j]) {
-							NGE[i] = A[j];
-							A[i + 1] = A[j];
-							break;
-						}
-
-						if (j == N) {
-							NGE[i] = -1;
-						}
-					}
-				}
+	for (int i = 1; i < N; i++) {
+		stack_size = stack.size();
+		for (int j = 0; (j < stack_size) && A[i] > A[stack.top()]; j++) {
+			if (A[stack.top()] < A[i]) {		// stack.top의 인덱스에 해당하는 A값보다 크면 오큰수
+				NGE[stack.top()] = A[i];		// NGE[stack.top()]에 오큰수를 넣는다.
+				stack.pop();					// top의 값은 pop
 			}
 		}
-		stack.pop();
+		stack.push(i);		// 마지막으로 현재 인덱스 번호 push
 	}
 
-	for (int i = 1; i <= N; i++) {
+	for (int i = 0; i < N; i++) {
 		cout << NGE[i] << " ";
 	}
-	cout << endl;
 
 	return 0;
 }
