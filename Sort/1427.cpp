2e71637909bb465689a4d243809f695c48a2aa5b
CYHONG_PC <mgs08041@naver.com>
2024-04-02 03:21:16 +0900

    정렬 - 소트인사이드(선택정렬) / 해결

--- Sort/1427.cpp
+++ Sort/1427.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
@@ -8,22 +10,41 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	vector<char> N(1'000'000'000);	// 정렬할 수 (<= 1,000,000,000)
+	string N;	// N을 string으로 한꺼번에 입력받고,
+	cin >> N;
 
-	int max = 0;
-	int i = 0;
-	while (cin >> N[i]) {
-		if (N[i] > max) {
-			max = N[i];
+	vector<char> num;
+
+	char input;
+
+	for (int i = 0; i < N.length(); i++) {
+		input = N[i];
+
+		if (input == '\n') {
+			break;
 		}
+
+		num.push_back(input);	// '\n'이 나올 때까지 vector에 저장
 	}
 
-	N[0] = max;
+	int maxIndex;
 
-	for (int i = 1; i < N.size(); i++) {
-		N[i] = 
+	int index = 0;
+	int temp = 0;
+	while (index < num.size()) {
+		maxIndex = max_element(num.begin() + index, num.end()) - num.begin();	// 최댓값이 담긴 iter 리턴
+
+		// 최댓값과 맨 앞 인덱스의 위치를 바꿈.
+		temp = num[maxIndex];
+		num[maxIndex] = num[index];
+		num[index] = temp;
+
+		index++;
 	}
-	
+
+	for (int i = 0; i < num.size(); i++) {
+		cout << num[i];
+	}
 
 	return 0;
 }
