0a343824df936105a02f59650abb0b85e3fba5b5
CYHONG_PC <mgs08041@naver.com>
2024-04-04 11:04:12 +0900

    정렬 - ATM (삽입 정렬) 재풀이(이전 풀이는 버블정렬로 품)

--- Sort/11399.cpp
+++ Sort/11399.cpp
@@ -13,24 +13,43 @@ int main() {
 	int count = 0;			// 총 인출 시간
 
 	cin >> N;
-	
+
 	for (int i = 0; i < N; i++) {
 		cin >> P[i];
 	}
 
-	int index = 0;
-	int temp;
+	int target_index = 1;
+	int insert_index = 0;
+	int insert_value = P[1];
+	bool is_change = false;
 
 	// 삽입 정렬
-	while (index < N) {
-		for (int i = 0; i < index; i++) {
-			if (P[index] < P[i]) {
-				temp = P[i];
-				P[i] = P[index];
-				P[index] = temp;
+	while (target_index < N) {
+		// 삽입할 위치 찾기
+		for (int i = 0; i < target_index; i++) {
+			if (P[target_index] < P[i]) {
+				insert_index = i;
+				insert_value = P[target_index];
+				is_change = true;
+				break;
 			}
 		}
-		index++;
+
+		// 정렬 대상값(target)의 위치가 그대로일 경우 다음 대상으로 넘어감
+		if (is_change == false) {
+			target_index++;
+			continue;
+		}
+
+		// target이 들어갈 위치 ~ target의 현재 위치까지 뒤로 한 칸씩 밀기
+		for (int i = target_index; i > insert_index; i--) {
+			P[i] = P[i - 1];
+		}
+		// target 삽입 위치에 target 값 넣기
+		P[insert_index] = insert_value;
+
+		target_index++;
+		is_change = false;
 	}
 
 	// 총 인출 소요 시간 계산
