1caf96da53b280dd16c00905ef249a8f7b7b766f
CYHONG_PC <mgs08041@naver.com>
2024-04-08 04:12:24 +0900

    정렬 - 수 정렬하기2 (합병정렬) / 해결

--- Sort/2751.cpp
+++ Sort/2751.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
+
+void merge(vector<int>& num, int start, int end);
+
+int N;	// 熱曖 偎熱
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N;		// 熱曖 偎熱
 	cin >> N;
 
 	vector<int> num(N);
@@ -18,11 +20,58 @@ int main() {
 		cin >> num[i];
 	}
 
-	sort(num.begin(), num.end());
+	merge(num, 0, N - 1);
 
 	for (int i = 0; i < N; i++) {
-		cout << num[i] << endl;
+		cout << num[i] << \n";
 	}
-	
+
 	return 0;
+}
+
+void merge(vector<int>& num, int start, int end) {
+	if (start == end) {
+		return;
+	}
+
+	int median = (start + end) / 2;
+
+	merge(num, start, median);
+	merge(num, median + 1, end);
+
+	vector<int> temp(N);
+	int tempIndex = start;
+
+	for (int i = start; i <= end; i++) {
+		temp[i] = num[i];
+	}
+
+	int point1 = start;
+	int point2 = median + 1;
+
+	while (point1 <= median && point2 <= end) {
+		if (num[point1] <= num[point2]) {
+			temp[tempIndex] = num[point1];
+			tempIndex++, point1++;
+		}
+		else {
+			temp[tempIndex] = num[point2];
+			tempIndex++, point2++;
+		}
+
+		if (point1 > median) {
+			for (int i = point2; i <= end; i++) {
+				temp[tempIndex++] = num[i];
+			}
+		}
+		else if (point2 > end) {
+			for (int i = point1; i <= median; i++) {
+				temp[tempIndex++] = num[i];
+			}
+		}
+	}
+
+	for (int i = start; i <= end; i++) {
+		num[i] = temp[i];
+	}
 }
